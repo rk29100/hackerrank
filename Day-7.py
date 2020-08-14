809f675b8296d57cb2b966eab9a5820e2c9cfbb1
rk29100 <69382712+rk29100@users.noreply.github.com>
2020-08-14 13:57:58 +0530

    updated recently

---
+++ Day-7.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    input()
+    arr = str(input()).split(" ")
+    arr.reverse()
+
+    for num in arr:
+        print(num + " ", end="")
